fd2a2b1baf25d511e19df8b12c317d96749512d3
Mason Watson <mkw@Masons-MBP.netgear.com>
2018-08-20 23:22:32 -0400

    install sass; work on organization

--- client/containers/FeatureCategory.js
+++ client/containers/FeatureCategory.js
@@ -12,18 +12,17 @@ class FeatureCategory extends Component {
     this.renderFeatureCategory = this.renderFeatureCategory.bind(this)
   }
 
-  // Fetch recipes when component is mounted
   componentDidMount() {
     const API_URL = 'http://localhost:3000/api/featurecategory/?list';
     this.props.fetchFeatureCategories(API_URL);
   }
-// Function to render our recipe
+
   renderFeatureCategory() {
     return _.map(this.props.featureCategories, featureCategory => {
       // Check if there is an image to be displayed
       const img = featureCategory.image ? featureCategory.image.filename : '';
 
-      // Make sure we show only published recipes
+      // Make sure we show only published categories
       if (featureCategory.state = "published") {
       return (
         <div>
---
+++ client/containers/NavBarContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class NavBarContainer extends Component {
+
+}
+
+export default NavBarContainer;
